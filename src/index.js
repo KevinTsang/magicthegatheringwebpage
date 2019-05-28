ca3cd60c0706ffa7f0221762949893235d66449e
KevinTsang <kevin@tsangfamily.us>
2019-05-27 17:45:41 -0700

    Added infinite scrolling

--- src/index.js
+++ src/index.js
@@ -1,46 +1,66 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Card from '../src/shared/card/card';
-import LoadingIndicator from '../src/shared/loading-indicator/loading-indicator';
-
-const mtg = require('mtgsdk');
 
 
-class MainPage extends Component {
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             cards: [],
-            loading: true,
+            pageNumber: 0,
+            hasMore: true,
+            totalCount: 0,
         };
+
+        this.loadCards = this.loadCards.bind(this);
+        this.hasMore = this.hasMore.bind(this);
     }
 
-    componentDidMount() {
-        mtg.card.where({ type: 'creature', page: 1, pageSize: 20 }).then((cards) => {
+    hasMore() {
+        return this.state.cards.length < this.state.totalCount;
+    }
+
+    loadCards(pageNumber) {
+        fetch('https://api.magicthegathering.io/v1/cards?type=creature&pageSize=20&page=' + pageNumber).then((results) => {
+            if (pageNumber === 0) {
+                this.setState({
+                    totalCount: results.headers.get('Total-Count')
+                });
+            }
+            return results.json();
+        }).then((cardArray) => {
+            const cards = cardArray.cards;
             this.setState({
-                cards: cards,
-                loading: false,
+                cards: this.state.cards.concat(cards),
+                pageNumber: this.state.pageNumber + 1,
             });
         });
     }
 
     render() {
         return (
-            <div className="container">
-                {
-                    this.state.loading ? (<LoadingIndicator />) :
-                    this.state.cards.map((card, i) => {
-                        return (<Card key={card.id} card={card} />);
-                    })
-                }
-            </div>
+            <InfiniteScroll
+                pageStart={this.state.pageNumber}
+                initialLoad={true}
+                loadMore={this.loadCards}
+                hasMore={this.state.hasMore}
+                loader={<div key={'loading'}><i className="fa fa-spinner fa-spin" />Loading...</div>}
+                >
+                <div className="container">
+                    {this.state.cards.map((card) => {
+                        return (<Card key={card.id} card={card}/>);
+                    })}
+                </div>
+            </InfiniteScroll>
         );
     }
 }
 
 ReactDOM.render(
-    <MainPage />,
+    <App />,
     document.getElementById('root')
 );
